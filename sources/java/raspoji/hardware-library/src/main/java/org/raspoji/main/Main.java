1b65649d4347fbce0a4e619b6bc6791a893c0a4e
Ronan <ronan.quintin@gmail.com>
2021-02-03 14:15:41 +0100

    First simple test

---
+++ sources/java/raspoji/hardware-library/src/main/java/org/raspoji/main/Main.java
@@ -0,0 +1,23 @@
+package org.raspoji.main;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final GpioController gpio = GpioFactory.getInstance();
+		
+		GpioPinDigitalOutput  led = gpio.provisionDigitalOutputPin(
+				RaspiPin.GPIO_00,             // PIN NUMBER
+				"led",
+				PinState.LOW
+				); // PIN RESISTANCE (optional)
+		
+		led.pulse(1000);
+	}
+
+}
