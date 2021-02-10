08e1826c14360aa77ae2db3678bd3a5f925e005b
Ronan <ronan.quintin@gmail.com>
2021-02-10 15:12:20 +0100

    Update HCSR04.java

--- sources/java/raspoji/hardware-library/src/main/java/org/raspoji/sensors/impl/HCSR04.java
+++ sources/java/raspoji/hardware-library/src/main/java/org/raspoji/sensors/impl/HCSR04.java
@@ -25,7 +25,8 @@ public class HCSR04 implements Sensor{
 	private  GpioPinDigitalInput echo;
 	
 	public Quantity doMeasure() throws MeasureException {
-		
+		long echoPulseStartTime = 0;
+		long echoPulseEndTime = 0;
 		
 		// Send an impulse during 10 micro seconds
 		logger.debug("Set trigger to high");
@@ -41,11 +42,13 @@ public class HCSR04 implements Sensor{
 		this.trigger.low();
 		
 		logger.debug("Waiting that echo goes from low to high, then low an measuring nano time interval");
-		while(echo.isLow()) {} // Waiting to have an high signal (wait until isn't low anymore)
-		long echoPulseStartTime = System.nanoTime();
+		while(echo.isLow()) {
+			echoPulseStartTime = System.nanoTime();
+		} // Waiting to have an high signal (wait until isn't low anymore)
 		
-		while(echo.isHigh()) {} // Then waiting that the signal goes down (wait until isn't high anymore)
-		long echoPulseEndTime = System.nanoTime();
+		while(echo.isHigh()) { // Then waiting that the signal goes down (wait until isn't high anymore)
+			echoPulseEndTime= System.nanoTime();
+		}
 		
 		long totalEchoDuration = echoPulseEndTime -echoPulseStartTime;
 	
